4513822272261bd97a8c75ece7acb95865ca624a
rafal <rafal_markowski@onet.pl>
2018-12-20 02:24:45 +0100

    Filtrowanie listy

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ export default class App extends React.Component {
         this.state.filteredContacts = this.state.contacts;
     }
 
+    filterContacts = e => {
+        const search = e.target.value.toLowerCase();
+        const contacts = this.state.contacts;
+        this.setState({
+            filteredContacts: contacts.filter(el => el.toLowerCase().indexOf(search) === 0)   
+        });
+    }
+
     render() {
-        const { filteredContacts } = this.state;
+        const filteredContacts = this.state.filteredContacts;
         
         return (
             <div>
                 <label className="c-filter">
-                    <input type="search" className="c-filter__input" />
+                    <input type="search" className="c-filter__input" onInput={this.filterContacts} />
                 </label>
                 <ul className="c-contacts">
                     { filteredContacts.map(el => <li key={el} className="c-contact__item">{el}</li>) }
